c62a8e07783f9465dcae2507b4652c4c7faf807c
bharadwaja2000 <nandivada.s@northeastern.edu>
2024-07-24 15:56:19 -0400

    reset django

---
+++ company_directory/myapp/src/ContactDetails.js
@@ -0,0 +1,17 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ContactDetails() {
+
+  return (
+    <section id="contact">
+      <h1>Get in Touch</h1>
+      <p>
+      k.vanaja@northeastern.edu
+      </p>
+    </section>
+  );
+}
+
+export default ContactDetails;
