1f87aa513c2657f9193012185b0ddacd6bfd0a2c
bharadwaja2000 <nandivada.s@northeastern.edu>
2024-07-26 14:29:10 -0400

    Map included

--- company_directory/myapp/src/CompanyDetails.js
+++ company_directory/myapp/src/CompanyDetails.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 import { fetchData } from './api';
+
+// Fix default icon issue with Leaflet in React
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 const CompanyDetails = () => {
     const { id } = useParams();
@@ -46,10 +57,27 @@ const CompanyDetails = () => {
         return <div>Error: {error}</div>;
     }
 
+    if (!company) {
+        return <div>No company data available</div>;
+    }
+
     return (
         <div>
             <h1>{company.name}</h1>
             <p>{company.address}</p>
+            <div style={{ height: '400px' }}>
+                <MapContainer center={[company.latitude, company.longitude]} zoom={13} style={{ height: '100%', width: '100%' }}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Marker position={[company.latitude, company.longitude]}>
+                        <Popup>
+                            {company.name} <br /> {company.address}
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
             <h2>Locations</h2>
             <ul>
                 {locations.map(location => (
